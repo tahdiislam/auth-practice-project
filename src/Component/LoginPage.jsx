56a4bfa91234f8d37a0a6c0414be72a510f528c0
tahdi-islam <tshdi60@gmail.com>
2022-10-17 01:44:57 +0600

    add login page

--- src/Component/LoginPage.jsx
+++ src/Component/LoginPage.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
+    // form submit handle
+    const handleSubmitForm = event => {
+        event.preventDefault()
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+    } 
     return (
         <div>
-            <h1>This is login page</h1>
+            <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col">
+                <div className="text-center lg:text-left">
+                <h1 className="text-5xl font-bold">Log In</h1>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <form onSubmit={handleSubmitForm} className="card-body">
+                    <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input type="email" name='email' placeholder="email" className="input input-bordered" required/>
+                    </div>
+                    <div className="form-control">
+                    <label className="label">
+                        <span className="label-email">Password</span>
+                    </label>
+                    <input type="password" name='password' placeholder="password" className="input input-bordered" required/>
+                    <label className="label">
+                        <Link to='/' className="label-text-alt link link-hover">Forgot password?</Link>
+                    </label>
+                    </div>
+                    <div className="form-control mt-6">
+                    <button type='submit' className="btn btn-primary">Log In</button>
+                    </div>
+                </form>
+                </div>
+            </div>
+            </div>
         </div>
     );
 };
--- src/Component/Register.jsx
+++ src/Component/Register.jsx
@@ -38,7 +38,7 @@ const Register = () => {
                     </label>
                     <input type="password" name='password' placeholder="password" className="input input-bordered" required/>
                     <label className="label">
-                        <Link to='/' className="label-text-alt link link-hover">Forgot password?</Link>
+                        <Link to='/login' className="label-text-alt link link-hover">log in here</Link>
                     </label>
                     </div>
                     <div className="form-control mt-6">
